4d0fb1f8547d497c73179408ead7e4d6d92b8f44
Tyler <insomniac34@gmail.com>
2014-10-23 21:35:42 -0400

    updated hangmantest

--- HangMan/HangmanTest.js
+++ HangMan/HangmanTest.js
@@ -5,8 +5,12 @@
 describe("game start", function() {
 	beforeEach(module('JHangman'));
 	
+	/* 
+		Before each test, we need to use Angular's inject() function to insert any dependencies that the code requires.
+		In order for each individual unit test to receive these injected values, we need to declare any dependencies as global values, in this case "var scope, theController" are our globals.
+		We pass Angular variables into the function (denoted by '$'), and assign "scope" to the controller's scope and theController to the value of the $controller variable which represents our hangman controller function
+	*/
 	var scope, theController;
-
 	beforeEach(inject(function($rootScope, $controller, $httpBackend) {
 		scope = $rootScope.$new();
 		theController = $controller("HangmanController", { $scope: scope});
@@ -29,10 +33,6 @@ describe("game start", function() {
 		expect(scope.guessed.letters).toBeUndefined();
 		expect(scope.notifications).toEqual([{msg: 'Welcome!', type: 'success'}]);
 		expect(scope.words).toEqual([]);
-	});
-
-	it("", function() {
-
 	});
 });
 
